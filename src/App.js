460ab7568e268204e43e0f45a33fd6c7ebed5dc6
Arun Kumar <arunkumar413@gmail.com>
2022-11-27 18:52:03 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import "./styles.css";
+import webpack from "webpack";
+import { path } from "path";
+export default function App() {
+  // https://webpack.js.org/api/node
+
+  let compiler = webpack({
+    entry: "./src/index.js",
+    output: {
+      path: path.resolve(__dirname, "dist")
+    }
+  });
+
+  compiler.run(function (err, stats) {
+    console.log(stats);
+
+    if (err) {
+      console.error(err.stack || err);
+      if (err.details) {
+        console.error(err.details);
+      }
+    }
+    const info = stats.toJson();
+    if (stats.hasErrors()) {
+      console.error(info.errors);
+    }
+  });
+
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+    </div>
+  );
+}
